df3d321c32a2bda09f19e8abf0fa14eefa5d460a
Jared Tucker <jntucker02@gmail.com>
2024-03-20 18:14:23 +1100

    Connected Signup/Avatar to Auth Session

--- client/components/ui/AuthProfile.tsx
+++ client/components/ui/AuthProfile.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import React from "react";
 import { Button } from "./button";
 import { Avatar } from "./avatar";
 import { BgTransitionButton } from "./AnimatedButton";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 function AuthProfile() {
+  const { data: session } = useSession();
   //todo: Implement User Avatar once Authentication has been implemented
+  if (session) {
+    return (
+      <>
+        {session.user?.name}
+        <Button className="px-10">
+          <Link href={"/auth/logout"}>Sign Out</Link>
+        </Button>
+      </>
+    );
+  }
   return (
     <BgTransitionButton className="px-10">
-      <Link href={"/auth"}>Sign Up</Link>
+      <Link href={"/auth/register"}>Sign Up</Link>
     </BgTransitionButton>
   );
 }
