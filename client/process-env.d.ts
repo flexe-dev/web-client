3f15d3f917c417c19345761c772af006cf7d89a5
Jared Tucker <jntucker02@gmail.com>
2024-03-22 19:12:22 +1100

    Add Typing to ENV

---
+++ client/process-env.d.ts
@@ -0,0 +1,18 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      PORT: string;
+      NEXTAUTH_URL: string;
+      NEXTAUTH_SECRET: string;
+      GITHUB_ID: string;
+      GITHUB_SECRET: string;
+      GOOGLE_ID: string;
+      GOOGLE_SECRET: string;
+      MONGODB_URI: string;
+      NEXT_API_URL: string;
+    }
+  }
+}
