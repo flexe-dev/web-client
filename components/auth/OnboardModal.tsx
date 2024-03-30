4507aaa52717166ad2be243bd94410a426c06d55
Jared Tucker <jntucker02@gmail.com>
2024-03-30 21:40:41 +1100

    Support Image Upload and Serving

--- components/auth/OnboardModal.tsx
+++ components/auth/OnboardModal.tsx
@@ -6,8 +6,9 @@ import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { Button } from "../ui/button";
 import { OnboardForm } from "../forms/OnboardForm";
+import { useAccount } from "../context/AccountProvider";
 export const OnboardModal = () => {
-  const user = useSession().data?.user;
+  const { user } = useAccount();
 
   //Only performing state operations due to Hydration Issues
   const [modalOpen, setModalOpen] = useState<boolean>(false);
@@ -22,7 +23,7 @@ export const OnboardModal = () => {
   return (
     <Dialog open={modalOpen}>
       <DialogContent
-        className="min-w-[50rem]"
+        className="w-full md:min-w-[45rem] lg:min-w-[50rem] "
         hasCloseButton={false}
         onInteractOutside={(e) => e.preventDefault()}
       >
@@ -30,8 +31,13 @@ export const OnboardModal = () => {
           <DialogHeader className="text-3xl font-semibold">
             Welcome to designs.dev
           </DialogHeader>
-          <DialogDescription className="text-secondary-foreground">
-            Lets grab some more details to finish setting up your account
+          <DialogDescription className="text-secondary-foreground flex flex-col">
+            <span>
+              Lets grab some more details to finish setting up your account
+            </span>
+            <span className="text-xs text-secondary-header mt-1">
+              Your information can be changed in the settings page at any time
+            </span>
           </DialogDescription>
           <OnboardForm onSuccess={handleFinishOnboard} />
         </>
