81eb5c5f2b2e2b8070030735e787a84ca0e65570
Jared Tucker <jntucker02@gmail.com>
2024-03-31 23:08:12 +1100

    Added Links to Dropdown Items

--- components/ui/User/UserDropdown.tsx
+++ components/ui/User/UserDropdown.tsx
@@ -11,6 +11,7 @@ import { User } from "@prisma/client";
 import { CogIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import { LogoutIcon } from "../../icons/LogoutIcon";
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 interface UserDropdownProps {
   children: React.ReactNode;
   user: User;
@@ -28,14 +29,18 @@ export const UserDropdown = ({ children, user }: UserDropdownProps) => {
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="flex space-x-3">
-          <UserCircleIcon className="stroke-secondary-header w-6 h-6" />
-          <span>Account</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="flex space-x-3">
-          <CogIcon className="stroke-secondary-header w-6 h-6" />
-          <span>Settings</span>
-        </DropdownMenuItem>
+        <Link href={"/profile"}>
+          <DropdownMenuItem className="flex space-x-3">
+            <UserCircleIcon className="stroke-secondary-header w-6 h-6" />
+            <span>Account</span>
+          </DropdownMenuItem>
+        </Link>
+        <Link href={"/settings"}>
+          <DropdownMenuItem className="flex space-x-3">
+            <CogIcon className="stroke-secondary-header w-6 h-6" />
+            <span>Settings</span>
+          </DropdownMenuItem>
+        </Link>
         <DropdownMenuSeparator />
         <DropdownMenuItem className="flex space-x-3" onClick={() => signOut()}>
           <LogoutIcon className="stroke-secondary-header" />
