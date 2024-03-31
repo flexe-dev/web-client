7859acecfc56fe6c97bd339ee1c9065be5e6e8de
Jared Tucker <jntucker02@gmail.com>
2024-03-31 18:15:34 +1100

    Change imports

--- components/ui/AuthProfile.tsx
+++ components/ui/AuthProfile.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-import { Button } from "./button";
-import { Avatar } from "./avatar";
+
 import { BgTransitionButton } from "./AnimatedButton";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-import UserAvatar from "./UserAvatar";
+import UserAvatar from "./User/UserAvatar";
 function AuthProfile() {
   const { data: session } = useSession();
   //todo: Implement User Avatar once Authentication has been implemented
