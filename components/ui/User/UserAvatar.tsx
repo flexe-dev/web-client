3b1c48e0f99a39f1bc62944bc6ec7bd8fae048a4
Jared Tucker <jntucker02@gmail.com>
2024-03-31 18:15:41 +1100

    Changed folder position

---
+++ components/ui/User/UserAvatar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../avatar";
+import { useAccount } from "../../context/AccountProvider";
+import { UserDropdown } from "./UserDropdown";
+
+function UserAvatar() {
+  const { user } = useAccount();
+
+  if (user) {
+    return (
+      <div className="flex items-center">
+        <h2 className="mr-4 md:hidden">{user.name}</h2>
+        <UserDropdown user={user}>
+          <Avatar className="border cursor-pointer hover:brightness-75 transition-all mr-2">
+            <AvatarImage className="object-cover" src={user.image} />
+            <AvatarFallback>
+              {user?.name
+                ?.split(" ")
+                .map((n) => n[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+        </UserDropdown>
+      </div>
+    );
+  }
+}
+
+export default UserAvatar;
