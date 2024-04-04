91518f1ea0da46f4da0b293bbbac0cd616a62d7d
Jared Tucker <jntucker02@gmail.com>
2024-04-04 19:06:55 +1100

    Placeholder Tile Component

---
+++ components/ui/GalleryTile.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Image from "next/image";
+import PlaceholderImage from "@/public/original-e6f547fe1fba8636ec3500af590b14ef.jpg";
+export const GalleryTile = () => {
+  return <div>GalleryTile</div>;
+};
