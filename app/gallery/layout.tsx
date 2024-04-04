fda1b4e711d7c7377259ac870d6632aef767b0c7
Jared Tucker <jntucker02@gmail.com>
2024-04-04 19:07:31 +1100

    Update Gallery Page

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import GallerySidebar from "@/components/ui/GallerySidebar";
+import GallerySidebar from "@/components/ui/Gallery/GallerySidebar";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,9 +8,9 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <main className="flex h-screen">
+    <main className="flex relative">
       <GallerySidebar />
-      <section className="flex flex-grow px-24 py-8">{children}</section>
+      <section className="flex flex-grow px-24 py-8 ">{children}</section>
     </main>
   );
 };
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
   return (
     <>
-      <div className="flex w-full justify-center">
+      <div className="flex w-full h-[200dvh] justify-center">
         <h1 className="text-4xl font-bold">Gallery</h1>
       </div>
     </>
