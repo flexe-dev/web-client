2c01d8ea8f79e79ee7be8f57546d3aa50c8e95d0
Jared Tucker <jntucker02@gmail.com>
2024-04-05 19:04:42 +1100

    Update Grouping

--- components/ui/AnimatedButton.tsx
+++ components/ui/AnimatedButton.tsx
@@ -12,12 +12,12 @@ export const BgTransitionButton = (props: ButtonWrapperProps) => {
     <Button
       variant={"outline"}
       className={cn(
-        "relative overflow-hidden group border-inverted hover:bg-transparent",
+        "relative overflow-hidden group/button border-inverted hover:bg-transparent",
         className
       )}
     >
-      <div className="h-full w-full transition-transform scale-x-100 group-hover:scale-x-0 bg-inverted absolute z-10 duration-300 origin-left group-hover:origin-right" />
-      <div className="z-20 text-inverted-foreground group-hover:text-inverted duration-500">
+      <div className="h-full w-full transition-transform scale-x-100 group-hover/button:scale-x-0 bg-inverted absolute z-10 duration-300 origin-left group-hover:origin-right" />
+      <div className="z-20 text-inverted-foreground group-hover/button:text-inverted duration-500">
         {children}
       </div>
     </Button>
