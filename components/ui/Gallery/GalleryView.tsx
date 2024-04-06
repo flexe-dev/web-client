2e4013ee8ecfaab358b269736338333efe05b43e
Jared Tucker <jntucker02@gmail.com>
2024-04-06 17:32:16 +1100

    Updated Layout of Gallery View

--- components/ui/Gallery/GalleryView.tsx
+++ components/ui/Gallery/GalleryView.tsx
@@ -1,25 +1,51 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
+import { LayoutGroup, motion } from "framer-motion";
+import { GalleryTile } from "./GalleryTile";
+import { useAccount } from "@/components/context/AccountProvider";
+import PlaceholderImage from "@/public/original-2f558c29eac9dc162da8e1ab9efe3d53.png";
+import { ImageProps } from "next/image";
 const GalleryView = () => {
+  const { user } = useAccount();
+  const image: ImageProps = {
+    src: PlaceholderImage,
+    alt: "Placeholder Image",
+  };
+  if (!user) return null;
   return (
-    <div className="flex flex-col w-full min-h-screen items-center">
+    <motion.section
+      layout
+      className="py-12 px-4 flex relative flex-col w-full min-h-screen items-center"
+    >
       <motion.h1 layout key={"gallery-header"} className="text-4xl font-bold">
         Gallery
       </motion.h1>
       <motion.h2
         layout
         key={"gallery-sub-title"}
-        className="text-center h-32 my-2 text-lg text-secondary-header"
+        className="text-center my-2 h-24 max-w-screen-md text-lg text-secondary-header"
       >
         A handpicked curated collection of special Portfolios, Projects,
         Animations and overall cool designs that you may find inspiration from.
       </motion.h2>
-      <section>
-        
-      </section>
-    </div>
+      <motion.section
+        layout="preserve-aspect"
+        className="relative flex flex-wrap justify-center p-4 items-center border-t group/gallery-tile"
+        key={"gallery-tile-section"}
+      >
+        {Array.from({ length: 12 }).map((_, index) => (
+          <GalleryTile
+            index={index}
+            key={`gallery-tile-${index}`}
+            creator={user}
+            title="Placeholder Title"
+            link="/inspiration"
+            image={image}
+          />
+        ))}
+      </motion.section>
+    </motion.section>
   );
 };
 
