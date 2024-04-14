767219f75007e8129ec1d32ae458f4afdd5846ce
Jared Tucker <jntucker02@gmail.com>
2024-04-14 17:23:10 +1000

    Updated Profile Layout to Ingest Provider and Render appropriate components

---
+++ app/[username]/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import ProfileHeader from "../../components/profile/ProfileHeader";
+import ProfileContent from "@/components/profile/ProfileContent";
+import { ProviderViewerProvider } from "@/components/context/UserProfileProvider";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <main className="min-h-screen">
+      <ProviderViewerProvider>
+        <div className="max-w-6xl mx-auto px-4 py-8 flex flex-col lg:flex-row gap-8 relative">
+          <ProfileHeader />
+          <ProfileContent>{children}</ProfileContent>
+        </div>
+      </ProviderViewerProvider>
+    </main>
+  );
+};
+
+export default Layout;
