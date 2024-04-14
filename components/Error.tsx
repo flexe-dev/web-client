211b60245a61c29bf5a0f6dfceebefaecf8eb150
Jared Tucker <jntucker02@gmail.com>
2024-04-14 20:33:21 +1000

    Error component for misroutes

--- components/Error.tsx
+++ components/Error.tsx
@@ -3,10 +3,12 @@ import React from "react";
 
 const ErrorPage = () => {
   return (
-    <section className="flex flex-col">
+    <section className="flex relative flex-auto flex-col items-center justify-center">
       <h1>Sorry this page could not be found</h1>
       <h2>The link you followed may be incorrect</h2>
-      <Link href={"/"}>Go back to the home page</Link>
+      <Link className="underline" href={"/"}>
+        Go back to the home page
+      </Link>
     </section>
   );
 };
