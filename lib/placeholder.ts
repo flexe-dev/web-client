563be13478c686ed9f3ce471adc49d8a0d55ab9f
Jared Tucker <jntucker02@gmail.com>
2024-04-25 21:12:56 +1000

    Revert "Removed Some Images to Maintain Storage"
    This reverts commit 8fe45ac6a69859bc26aa634115c659089a93085d.

--- lib/placeholder.ts
+++ lib/placeholder.ts
@@ -2,6 +2,9 @@ import { ImageProps } from "next/image";
 import Placeholder1 from "@/public/Default2.jpg";
 import Placeholder2 from "@/public/Default3.jpg";
 import Placeholder3 from "@/public/Default4.jpg";
+import Placeholder4 from "@/public/Default5.jpg";
+import Placeholder5 from "@/public/Default6.png";
+import Placeholder6 from "@/public/Default7.jpg";
 import Placeholder7 from "@/public/Default8.jpg";
 import Placeholder8 from "@/public/Default9.png";
 
@@ -9,6 +12,9 @@ export const images: ImageProps[] = [
   Placeholder1,
   Placeholder2,
   Placeholder3,
+  Placeholder4,
+  Placeholder5,
+  Placeholder6,
   Placeholder7,
   Placeholder8,
 ].map((image) => ({ src: image, alt: "Placeholder" }));
