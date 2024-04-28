5daf558dc3c50cdc4e6b0b36ebccff2fa055d725
Jared Tucker <jntucker02@gmail.com>
2024-04-28 19:00:39 +1000

    Update Import Path

--- app/new/media/page.tsx
+++ app/new/media/page.tsx
@@ -4,7 +4,7 @@ import UploadUserImages from "@/components/upload/UserUploadImage";
 import React from "react";
 import { PostContent } from "@prisma/client";
 import { CreatePost } from "@/lib/interface";
-import PostCreator from "@/components/creator/PostCreator";
+import PostCreator from "@/components/upload/PostCreator";
 
 const page = () => {
   const [uploadedFiles, setUploadedFiles] = React.useState<CreatePost[]>([]);
