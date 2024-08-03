f26b4792938cbc78ae17cc61942c25bbc1cae3c2
Jared Tucker <jtuc0006@student.monash.edu>
2024-08-03 16:22:22 +1000

    Remove out of scope files

--- server/flex-core/src/main/java/com/flexe/flex_core/entity/posts/media/PostUserMedia.java
+++
@@ -1,8 +0,0 @@
-package com.flexe.flex_core.entity.posts.media;
-
-import java.io.File;
-
-public class PostUserMedia {
-    private MediaContent content;
-    private File file;
-}
--- server/flex-core/src/main/java/com/flexe/flex_core/repository/post/PostNodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flexe.flex_core.repository.post;
-
-import com.flexe.flex_core.entity.nodes.posts.PostNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface PostNodeRepository extends Neo4jRepository<PostNode, String> {
-}
